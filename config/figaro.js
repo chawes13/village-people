9e655cebbc73b06607ce8b0eb786c757626c4402
Conor Hawes <conor@launchpadlab.com>
2020-05-04 20:23:44 -0500

    Exclude env vars via figaro

--- config/figaro.js
+++ config/figaro.js
@@ -3,14 +3,23 @@
 
 // These keys will break compilation if not defined.
 const REQUIRED_KEYS = [
-  // e.g. 'API_URL'
+  'API_URL',
 ]
 
-// These keys will be excluded from 
-// the proccess.env object in the compiled JS.
+// These keys will be excluded from
+// the process.env object in the compiled JS.
 const PRIVATE_KEYS = [
   'PROXIED_API_URL',
   'PROXIED_API_TOKEN',
+  'GCLOUD_PROJECT',
+  'GCLOUD_PRIVATE_KEY',
+  'GCLOUD_CLIENT_EMAIL',
+  'SPREADSHEET_ID',
+  'SHEET_NAME',
+  'FILTER_SHEET_NAME',
+  'SESSION_SECRET',
+  'CLAIM_ACCOUNT_TOKEN',
+  'APP_TOKEN',
 ]
 
 module.exports = { REQUIRED_KEYS, PRIVATE_KEYS }
