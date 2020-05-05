785bc757c974460841fccc3107cae1f9349015c2
Conor Hawes <conor@launchpadlab.com>
2020-05-05 08:34:08 -0500

    Update verification middleware to handle empty token

--- server/api/middleware.js
+++ server/api/middleware.js
@@ -1,9 +1,22 @@
 const { verify } = require('../utils')
 const { NODE_ENV, APP_TOKEN } = process.env
 
+const replace = (str, pattern, replacement) => {
+  if (!str) return ''
+  str.replace(pattern, replacement)
+}
+
 const isVerified = (req, res, next) => {
-  const bearerToken = req.headers.authorization.replace(/^Bearer /, '')
-  const verifiedToken = verify(bearerToken)
+  const bearerToken = replace(req.headers.authorization, /^Bearer /, '')
+  let verifiedToken = null
+
+  console.log(NODE_ENV)
+
+  try {
+    verifiedToken = verify(bearerToken)
+  } catch (e) {
+    // do nothing
+  }
 
   if (verifiedToken === APP_TOKEN || NODE_ENV === 'development') {
     next()
