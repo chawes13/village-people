9608eb33f909174c99174973ea4290a4df9fae68
Conor Hawes <conor@launchpadlab.com>
2020-05-09 10:35:19 -0500

    Modify message shown on about when signed in

--- src/js/main/home/views/About.js
+++ src/js/main/home/views/About.js
@@ -1,24 +1,34 @@
-import React from 'react'
-// import PropTypes from 'prop-types'
-// import * as Types from 'types'
-// import { onMount, waitFor } from 'lp-hoc'
-// import { selectors } from '../reducer'
-// import * as actions from '../actions'
-// import * as apiActions from 'api-actions'
+import React, { useMemo } from 'react'
+import { Link } from 'react-router-dom'
+import * as LS from 'local-storage'
 
 const propTypes = {}
 
 const defaultProps = {}
 
 function About() {
+  const isAuthenticated = useMemo(() => !!LS.getToken(), [])
   return (
     <div>
       <p>Welcome to the Village People!</p>
-      <p>
-        This is an application that provides management with a shared address
-        book for the staff in the Village. If you would like access, please send
-        a request to your administrator.
-      </p>
+      {isAuthenticated && (
+        <>
+          <p>
+            This is an application that provides management with a shared
+            address book for the staff in the Village.
+          </p>
+          <Link to="/contacts" className="button-primary">
+            View Contacts
+          </Link>
+        </>
+      )}
+      {!isAuthenticated && (
+        <p>
+          This is an application that provides management with a shared address
+          book for the staff in the Village. If you would like access, please
+          send a request to your administrator.
+        </p>
+      )}
     </div>
   )
 }
